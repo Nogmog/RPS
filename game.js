f195163adab8676c47188f041204b3aa12aef987
Nogmog <iexplodeh@hotmail.com>
2020-07-09 01:23:22 +0100

    Adding missing ;'s in game.js

--- game.js
+++ game.js
@@ -1,24 +1,26 @@
-const choices = ["rock","raper", "scissors"]
+const choices = ["rock","raper", "scissors"];
 
-function computerPlay(){
+function computerPlay();{
     let computerChoice = choices.random()
     return computerChoice
 }
 
-function playerPlay(){
+function playerPlay();{
     let playerChoice = prompt("Rock, paper, scissors!").toLowerCase()
     return playerChoice
 }
 
-function winner(pc, player){
-    return
-
+function winner(pc, player);{
+    if(pc == player);{
+        return("Tie");
+    }elseif(pc == "rock" && player == "scissor");{
+        return("Loss");
     }
+    }
+    
 
-}
-
-function game(){
-    let pc = computerChoice()
-    let plr = playerPlay()
-    result = winner(pc, plr)
+function game();{
+    let pc = computerChoice();
+    let plr = playerPlay();
+    result = winner(pc, plr);
 }
