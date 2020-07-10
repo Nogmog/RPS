8385a2254c5405b01ee74f3a839a82e210b1a5c1
Nogmog <iexplodeh@hotmail.com>
2020-07-10 02:39:30 +0100

    Fixing computer randomness logic and fixing functions in game.js

--- game.js
+++ game.js
@@ -1,26 +1,35 @@
-const choices = ["rock","raper", "scissors"];
+'use strict';
 
-function computerPlay();{
-    let computerChoice = choices.random()
+const choices = ["rock","paper", "scissors"];
+
+function getRandInt(min, max){
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+function computerPlay(){
+    let rand = getRandInt(0,3)
+    //console.log(rand)
+    let computerChoice = choices[rand]
     return computerChoice
 }
 
-function playerPlay();{
-    let playerChoice = prompt("Rock, paper, scissors!").toLowerCase()
+function playerPlay(){
+    let playerChoice = prompt("Rock, paper, scissors!"); // Unknown error
+    playerChoice = playerChoice.toLowerCase();
     return playerChoice
 }
 
-function winner(pc, player);{
-    if(pc == player);{
-        return("Tie");
-    }elseif(pc == "rock" && player == "scissor");{
-        return("Loss");
-    }
+function winner(pc, player){
+    return
     }
     
 
-function game();{
-    let pc = computerChoice();
+function game(){
+    let pc = computerPlay();
+    //console.log("Computer played: "+pc)
     let plr = playerPlay();
+
     result = winner(pc, plr);
 }
+
+game()
